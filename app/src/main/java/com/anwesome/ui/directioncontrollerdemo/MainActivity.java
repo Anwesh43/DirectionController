ead401f06b2807387f61afba8fc3f3c52aba0500
Anwesh43 <anweshthecool0@gmail.com>
2017-05-02 10:32:06 +0530

    Using DirectionController in app

--- app/src/main/java/com/anwesome/ui/directioncontrollerdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/directioncontrollerdemo/MainActivity.java
@@ -2,8 +2,10 @@ package com.anwesome.ui.directioncontrollerdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.anwesome.ui.directioncontroller.DirectionController;
+import com.anwesome.ui.directioncontroller.OnDirectionChangeListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView helloText = (TextView)findViewById(R.id.text_hello);
         DirectionController directionController = new DirectionController(this);
+        directionController.setOnDirectionChangeListener(new OnDirectionChangeListener() {
+            @Override
+            public void onDirectionChange(float dx, float dy) {
+                helloText.setX(helloText.getX()+dx*10);
+                helloText.setY(helloText.getY()+dy*10);
+            }
+        });
         directionController.show();
     }
 }
