74777b932a34dbd6673994acf4a6a6339e91dd5b
Anwesh43 <anweshthecool0@gmail.com>
2017-05-02 07:32:09 +0530

    Adding DirectionButtonOpaque startUpdating method, common method for drawingTriangle and handleTap method for directionButton

--- directioncontroller/src/main/java/com/anwesome/ui/directioncontroller/DirectionButton.java
+++ directioncontroller/src/main/java/com/anwesome/ui/directioncontroller/DirectionButton.java
@@ -7,6 +7,7 @@ import android.graphics.*;
  */
 public class DirectionButton {
     private float x,y,deg = 0,size;
+    private DirectionButtonOpaque directionButtonOpaque;
     public DirectionButton(float deg) {
         this.deg = deg;
     }
@@ -14,19 +15,29 @@ public class DirectionButton {
         this.x = x;
         this.y = y;
         this.size = size;
+        directionButtonOpaque = new DirectionButtonOpaque();
     }
     public void draw(Canvas canvas, Paint paint) {
         canvas.save();
-        canvas.translate(x,y);
+        canvas.translate(x+size/2,y+size/2);
         canvas.rotate(deg);
         paint.setColor(Color.parseColor("#BDBDBD"));
-        Path path = new Path();
-        path.moveTo(0,0);
-        path.lineTo(-size/2,0);
-        path.lineTo(0,-size);
-        path.lineTo(size/2,0);
-        canvas.drawPath(path,paint);
+        drawTriangle(canvas,paint);
         canvas.restore();
+    }
+    public boolean handleTap(float x,float y) {
+        boolean condition =  x>=this.x -size && x<=this.x+size && y>=this.y-size && y<=this.y+size;
+        if(directionButtonOpaque != null) {
+            directionButtonOpaque.startUpdating();
+        }
+        return condition;
+    }
+    private void drawTriangle(Canvas canvas,Paint paint) {
+        Path path = new Path();
+        path.lineTo(-size/2,size/2);
+        path.lineTo(0,-size/2);
+        path.lineTo(size/2,size/2);
+        canvas.drawPath(path,paint);
     }
     private class DirectionButtonOpaque {
         private float dir = 0,scale = 0;
@@ -35,11 +46,7 @@ public class DirectionButton {
             canvas.save();
             canvas.translate(0,size/2);
             canvas.scale(scale,scale);
-            Path path = new Path();
-            path.lineTo(-size/2,size/2);
-            path.lineTo(0,-size/2);
-            path.lineTo(size/2,size/2);
-            canvas.drawPath(path,paint);
+            drawTriangle(canvas,paint);
             canvas.restore();
         }
         public void update() {
@@ -50,6 +57,11 @@ public class DirectionButton {
             if(scale<=0) {
                 dir = 0;
                 scale = 0;
+            }
+        }
+        public void startUpdating() {
+            if(dir == 0 && scale == 0) {
+                dir = 1;
             }
         }
     }
