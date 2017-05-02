fbd886afb291d4df8d6ed0aadd9c7ad87166d8c0
Anwesh43 <anweshthecool0@gmail.com>
2017-05-02 09:50:42 +0530

    Using DirectionButton in app

--- app/src/main/java/com/anwesome/ui/directioncontrollerdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/directioncontrollerdemo/MainActivity.java
@@ -3,11 +3,15 @@ package com.anwesome.ui.directioncontrollerdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anwesome.ui.directioncontroller.DirectionController;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DirectionController directionController = new DirectionController(this);
+        directionController.show();
     }
 }
